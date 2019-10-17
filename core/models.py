f8b9ec511decc3bce74f4c7bca61ecc6f2f9e552
Rosana Rezende <contato@rosanarezende.com>
2019-10-17 08:37:42 -0300

    Criada tela de listagem de eventos

--- core/models.py
+++ core/models.py
@@ -16,3 +16,6 @@ class Evento(models.Model):
 
     def __str__(self):
         return self.titulo #sempre q alguém chamar o objeto título, volta o título
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y - %H:%M horas')
